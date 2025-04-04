f604515a601a2a6bd2470262bd5c8bc210c4b9dd
Luis Fernando Neu <luis.neu@gmail.com>
2025-04-04 10:53:10 -0300

    terraform for admin user and admin group

---
+++ modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+  type    = string
+  default = "dromedario"
+}
+
+variable "groupname" {
+  type    = string
+  default = "ext-admin"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "username" {
+  type    = string
+  default = "usuario"
+}
+
+variable "groupname" {
+  type    = string
+  default = "grupo"
+}
